12b8371174a8f1bd3dff1e3fd28fa894f59cb5de
Neal Rosen <neal.rosen@oath.com>
2020-11-30 15:30:18 -0500

    Updated license to Apache 2.0

--- src/sync.js
+++ src/sync.js
@@ -1,4 +1,4 @@
-/* Copyright Verizon Media, Licensed under the terms of the MIT license. See LICENSE file in project root for terms. */
+/* Copyright Verizon Media, Licensed under the terms of the Apache 2.0 license. See LICENSE file in project root for terms. */
 
 import state from "./state";
 import api from './api';
